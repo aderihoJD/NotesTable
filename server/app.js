2f4e19127d400c53ac56c0fe6ad26dda601e2f34
aderiho_ilya <ilya_aderiho@icloud.com>
2017-09-05 03:13:39 +0300

    MongoDB (mongoose) added. Config file setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,28 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
+
+import {serverPort} from "../config/default";
+import * as db from "./utils/DataBaseUtils";
+
+
+db.setUpConnection();
 
 const app = express();
 
-const server = app.listen(8080, ()=>{
-    console.log('Server is up and runnig on port 8080')
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+    db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+    db.createNote(req.body).then(data => res.send(data))
+});
+
+app.delete('/notes/:id', (req, res) => {
+    db.deleteNote(req.params.id).then(data => res.send(data));
+});
+
+const server = app.listen(serverPort, () => {
+    console.log(`Server is up and runnig on port: ${serverPort}`)
 });
---
+++ server/utils/DataBaseUtils.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+import "../models/Note";
+
+import {db} from "../../config/default";
+
+const Note = mongoose.model('Note');
+
+export function setUpConnection() {
+    mongoose.connect(`mongodb://${db.host}:${db.port}/${db.name}`);
+    console.log(`Connected to MongoDB on port: ${db.port}`);
+}
+
+export function listNotes() {
+    return Note.find();
+}
+
+export function createNote(data) {
+    const note = new Note({
+        title: data.title,
+        text: data.text,
+        color: data.color,
+        createdAt: new Date()
+    });
+
+    return note.save();
+}
+
+export function deleteNote(id){
+    return Note.findById(id).remove();
+}
